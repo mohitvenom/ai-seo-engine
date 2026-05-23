658888397a88ba80c39bb736603f69228f32071f
mohitvenom <slsharmakv04@gmail.com>
2026-05-24 01:42:33 +0530

    add vercel config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/ai-seo-engine/",
   server: {
     port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
   },
 });
